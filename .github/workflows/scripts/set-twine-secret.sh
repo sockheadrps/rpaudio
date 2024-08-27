e40893f7487c678c93fc97a9c4e6d7362be0d4fa
sockheadrps <r.p.skiles@gmail.com>
2024-08-26 21:40:42 -0400

    Modular Workflow builds
    New release workflow
    
    New release workflow
    
    New release workflow
    
    New release workflow
    
    New release workflow
    
    New release workflow
    
    New release workflow

---
+++ .github/workflows/scripts/set-twine-secret.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check if the branch is 'main'
+if [ "${GITHUB_REF#refs/heads/}" == "main" ]; then
+  echo "TWINE_PASSWORD=${{ secrets.PYPI_API_TOKEN }}" >> $GITHUB_ENV
+else
+  echo "TWINE_PASSWORD=${{ secrets.TEST_PYPI_API_TOKEN }}" >> $GITHUB_ENV
+fi
