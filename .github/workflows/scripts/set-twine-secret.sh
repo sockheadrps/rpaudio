4ed8588d929a03456151af60cf9f9a977cef6ffd
sockheadrps <r.p.skiles@gmail.com>
2024-08-27 00:13:44 -0400

    Seperated build actions
    Seperated build actions
    
    Seperated builds
    
    Seperated builds

--- .github/workflows/scripts/set-twine-secret.sh
+++ .github/workflows/scripts/set-twine-secret.sh
@@ -3,6 +3,8 @@
 # Check if the branch is 'main'
 if [ "${GITHUB_REF#refs/heads/}" == "main" ]; then
   echo TWINE_PASSWORD="secrets.PYPI_API_TOKEN" >> $GITHUB_ENV
+  echo "Release to PyPI"
 else
   echo TWINE_PASSWORD="secrets.TEST_PYPI_API_TOKEN" >> $GITHUB_ENV
+  echo "Release to Test PyPI"
 fi
