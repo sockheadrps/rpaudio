61ee5eccbfea9c7b87c0f2353de87e83c6c149d4
sockheadrps <r.p.skiles@gmail.com>
2024-08-26 22:02:26 -0400

    Twine pypi password script

--- .github/workflows/scripts/set-twine-secret.sh
+++ .github/workflows/scripts/set-twine-secret.sh
@@ -4,5 +4,5 @@
 if [ "${GITHUB_REF#refs/heads/}" == "main" ]; then
   echo TWINE_PASSWORD="secrets.PYPI_API_TOKEN" >> $GITHUB_ENV
 else
-  echo TWINE_PASSWORD=secrets.TEST_PYPI_API_TOKEN" >> $GITHUB_ENV
+  echo TWINE_PASSWORD="secrets.TEST_PYPI_API_TOKEN" >> $GITHUB_ENV
 fi
