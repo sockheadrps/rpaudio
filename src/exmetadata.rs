cbc4b22c032423bc98b7742bc6a145e2d5e50a5e
sockheadrps <r.p.skiles@gmail.com>
2024-09-02 11:49:47 -0400

    complete tests
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    Update release.yml
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    complete tests
    
    github actions kicked my ass
    
    test results

--- src/exmetadata.rs
+++ src/exmetadata.rs
@@ -171,25 +171,17 @@ pub fn extract_metadata(path: &Path) -> PyResult<MetaData> {
                 .read_from_path(path)
                 .map_err(|e| PyRuntimeError::new_err(format!("Failed to read tag: {}", e)));
 
-            match tag_result {
-                Ok(tag) => {
-                    let id3_tag_result: Result<Id3v2Tag, Box<dyn std::error::Error>> = Ok(Id3v2Tag::from(tag));
-                    match id3_tag_result {
-                        Ok(id3_tag) => {
-                            let metadata = id3_tag.metadata_fields();
-                            Ok(metadata)
-                        },
-                        Err(e) => {
-                            eprintln!("Failed to convert tag to Id3v2Tag: {}. Returning empty metadata.", e);
-                            Ok(MetaData::default())
-                        }
+                match tag_result {
+                    Ok(tag) => {
+                        let id3_tag = Id3v2Tag::from(tag);
+                        let metadata = id3_tag.metadata_fields();
+                        Ok(metadata)
+                    },
+                    Err(e) => {
+                        eprintln!("Failed to read tag: {}. Returning empty metadata.", e);
+                        Ok(MetaData::default())
                     }
-                },
-                Err(e) => {
-                    eprintln!("Failed to read tag: {}. Returning empty metadata.", e);
-                    Ok(MetaData::default())
                 }
-            }
         },
         Some("wav") => {
             let file = File::open(path).map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
