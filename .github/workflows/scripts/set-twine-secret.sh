85137e2759e74bd0d163686f036833cdc9a64fa2
sockheadrps <r.p.skiles@gmail.com>
2024-09-02 20:01:15 -0400

    0.0.5

--- .github/workflows/scripts/set-twine-secret.sh
+++ .github/workflows/scripts/set-twine-secret.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo "Branch name detected: ${GITHUB_REF#refs/heads/}"
 # Check if the branch is 'main'
 if [ "${GITHUB_REF#refs/heads/}" == "main" ]; then
-  echo TWINE_PASSWORD="secrets.PYPI_API_TOKEN" >> $GITHUB_ENV
+  echo "TWINE_PASSWORD=${{ secrets.PYPI_API_TOKEN }}" >> $GITHUB_ENV
   echo "Release to PyPI"
 else
-  echo TWINE_PASSWORD="secrets.TEST_PYPI_API_TOKEN" >> $GITHUB_ENV
+  echo "TWINE_PASSWORD=${{ secrets.TEST_PYPI_API_TOKEN }}" >> $GITHUB_ENV
   echo "Release to Test PyPI"
 fi
