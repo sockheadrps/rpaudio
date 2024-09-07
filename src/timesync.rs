f5aba91bb41c0831d6993aa77e005aa995cd2d4c
sockheadrps <r.p.skiles@gmail.com>
2024-09-07 07:21:53 -0400

    added fade out effect

--- src/timesync.rs
+++ src/timesync.rs
@@ -1,7 +1,4 @@
-use crate::AudioSink;
 use pyo3::prelude::*;
-use std::sync::Arc;
-use std::time::Duration;
 
 
 #[derive(Clone, Debug)]
@@ -17,17 +14,17 @@ pub struct FadeIn {
     pub apply_after: Option<f32>, 
 }
 
-const DEFAULT_DURATION: f32 = 5.0;
-const DEFAULT_START_VOL: f32 = 0.1;
-const DEFAULT_END_VOL: f32 = 1.0;
+const DEFAULT_FADE_IN_DURATION: f32 = 5.0;
+const DEFAULT_FADE_IN_START_VOL: f32 = 0.1;
+const DEFAULT_FADE_IN_END_VOL: f32 = 1.0;
 
 #[pymethods]
 impl FadeIn {
     #[new]
     #[pyo3(signature = (
-        duration=DEFAULT_DURATION,
-        start_vol=DEFAULT_START_VOL,
-        end_vol=DEFAULT_END_VOL,
+        duration=DEFAULT_FADE_IN_DURATION,
+        start_vol=DEFAULT_FADE_IN_START_VOL,
+        end_vol=DEFAULT_FADE_IN_END_VOL,
         apply_after=None
     ))]
     pub fn new(
@@ -37,28 +34,53 @@ impl FadeIn {
         apply_after: Option<f32>
     ) -> PyResult<Self> {
         Ok(FadeIn {
-            duration: duration.unwrap_or(DEFAULT_DURATION),
-            start_vol: start_vol.unwrap_or(DEFAULT_START_VOL),
-            end_vol: end_vol.unwrap_or(DEFAULT_END_VOL),
+            duration: duration.unwrap_or(DEFAULT_FADE_IN_DURATION),
+            start_vol: start_vol.unwrap_or(DEFAULT_FADE_IN_START_VOL),
+            end_vol: end_vol.unwrap_or(DEFAULT_FADE_IN_END_VOL),
             apply_after,
         })
     }
 }
 
-#[derive(Debug)]
-#[derive(Clone)]
+// Constants for default values
+const DEFAULT_FADE_OUT_DURATION: f32 = 5.0;
+const DEFAULT_FADE_OUT_START_VOL: f32 = 0.1;
+const DEFAULT_FADE_OUT_END_VOL: f32 = 1.0;
+
+#[derive(Clone, Debug)]
 #[pyclass]
 pub struct FadeOut {
     #[pyo3(get, set)]
     pub duration: f32,
+    #[pyo3(get, set)]
+    pub start_vol: f32,
+    #[pyo3(get, set)]
+    pub end_vol: f32,
+    #[pyo3(get, set)]
+    pub apply_after: Option<f32>,
 }
-
 
 #[pymethods]
 impl FadeOut {
     #[new]
-    pub fn new(duration: f32) -> Self {
-        FadeOut { duration }
+    #[pyo3(signature = (
+        duration=DEFAULT_FADE_OUT_DURATION,
+        start_vol=DEFAULT_FADE_OUT_START_VOL,
+        end_vol=DEFAULT_FADE_OUT_END_VOL,
+        apply_after=None
+    ))]
+    pub fn new(
+        duration: Option<f32>,
+        start_vol: Option<f32>,
+        end_vol: Option<f32>,
+        apply_after: Option<f32>
+    ) -> PyResult<Self> {
+        Ok(FadeOut {
+            duration: duration.unwrap_or(DEFAULT_FADE_OUT_DURATION),
+            start_vol: start_vol.unwrap_or(DEFAULT_FADE_OUT_START_VOL),
+            end_vol: end_vol.unwrap_or(DEFAULT_FADE_OUT_END_VOL),
+            apply_after,
+        })
     }
 }
 #[derive(Debug)]
