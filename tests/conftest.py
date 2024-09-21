4cc282693ae68fa14e76ecbab085521cb1a7c230
Ryan <39883642+sockheadrps@users.noreply.github.com>
2024-09-21 10:07:35 -0400

    Update pytest_output.txt
    Update release.yml
    
    Update conftest.py
    
    Update release.yml
    
    Update release.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,5 @@ import datetime
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_sessionfinish(session, exitstatus):
-    # Get the current UTC time as a Unix timestamp
-    now_utc = datetime.datetime.utcnow()
-    unix_timestamp = int(now_utc.timestamp())
-    formatted_time = now_utc.strftime("%Y-%m-%d %H:%M:%S")
-    
-    print(f"\n {unix_timestamp} - All tests completed.")
+    now = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"\n {now} - All tests completed.")
